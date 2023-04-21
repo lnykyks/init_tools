3e7ff43811fc63a987e68f27bed4053aa740ee06
linycs <63550832+lnykyks@users.noreply.github.com>
2023-04-21 19:56:18 +0800

    Update init_git.sh

--- shell/init_git.sh
+++ shell/init_git.sh
@@ -6,9 +6,10 @@ ssh-keygen -t rsa -C "lnykyks@gmail.com"
 git config --global -l # check info
 
 # init folder, then push
-git add .
 git init
 git remote add origin "the link"# push
+git add .
+git commit -m "init" # use init to create?
 git push -u origin master //将主分支本地的内容推送到GitHub上的远程仓库上
 
 # pull from github
@@ -16,3 +17,9 @@ git remote add origin <你github上仓库的url>
 git pull origin master
 
 git clone git@github.com:lnykyks/Logseq.git # for example
+
+# detach
+git remote remove origin # 或者直接删掉.git文件
+
+# authorization
+git -T git@github.com
