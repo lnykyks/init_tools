192c5321cade66e500f7be049f20b9b59a8b76a8
linycs <63550832+lnykyks@users.noreply.github.com>
2023-07-22 16:32:00 +0800

    Update init_conda.sh

--- shell/init_conda.sh
+++ shell/init_conda.sh
@@ -4,3 +4,9 @@ conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/f
 conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/conda-forge 
 conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/msys2/
 conda config --add channels https://mirrors.bfsu.edu.cn/anaconda/cloud/pytorch
+
+# 2.在.bashrc文件底部添加  
+# 为了避免与其他服务器用户产生命令冲突,使用自己的英文名+Python替代python 
+alias fortunePython='/root/anaconda3/bin/python'   
+# 这里写anaconda的安装路径
+export PATH="/root/anaconda3/bin:$PATH”
